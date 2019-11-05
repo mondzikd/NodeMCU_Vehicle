725c8903599b08ab5f80c317e842901ebec13354
pikinier20 <filip.zybala@gmail.com>
2019-11-05 11:45:09 +0100

    UDP works

--- NodeMCU_Vehicle.ino
+++ NodeMCU_Vehicle.ino
@@ -1,4 +1,5 @@
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 
 struct state{
   int rotate_A;       // range: -1023 to 1023
@@ -7,11 +8,29 @@ struct state{
   bool soft_stop_B;   // opposite to hard stop when rotate = 0
 };
 
+const int MAX_PACKET_SIZE = 256;
+const char MAGIC_NUMBER = 123;
+
 struct state vehicle = {0, 0, false, false};
 int AIN1, AIN2, PWMA;
 int BIN1, BIN2, PWMB;
+WiFiUDP Udp;
+unsigned int localUdpPort = 6111;
+char incomingPacket[MAX_PACKET_SIZE];
+bool paired = false;
+IPAddress pairedIp;
+unsigned int pairedPort;
 
 void setup() {
+  Serial.begin(115200);
+  WiFi.begin("Detonate-2137","roszki123");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Udp.begin(localUdpPort);
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
 //
 //  WiFi.mode(WIFI_STA);
 //  WiFi.disconnect();
@@ -52,9 +71,52 @@ void setup() {
 }
 
 void loop() {
-  tank_test();
+  //tank_test();
+  if(get_udp_packet()){
+    parse_udp_packet();
+    set_engines();
+  }
+  
+  
+}
+bool get_udp_packet(){
+    int packetSize = Udp.parsePacket();
+    int len = 0;
+    if(packetSize <= MAX_PACKET_SIZE && packetSize > 0) {
+      Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+      len = Udp.read(incomingPacket, MAX_PACKET_SIZE);
+      if(len > 0) incomingPacket[len] = '\0';
+      Serial.printf("UDP packet contents: %s\n", incomingPacket);    
+      }
+    return len > 0;
 }
 
+void parse_udp_packet(){
+  if(incomingPacket[0] != MAGIC_NUMBER) return;
+  if(paired && Udp.remoteIP() == pairedIp){
+    pairedPort = Udp.remotePort();
+  }else if(!paired){
+    paired = true;
+    pairedIp = Udp.remoteIP();
+    pairedPort = Udp.remotePort();
+  }else return;
+  if(incomingPacket[1] == 255){
+    paired = false;
+  }
+  int temp;
+  temp =  (incomingPacket[2] - 127) * 8;
+  if(temp >= -1023 && temp <= 1023) vehicle.rotate_A = temp;
+  temp =  (incomingPacket[3] - 127) * 8;
+  if(temp >= -1023 && temp <= 1023) vehicle.rotate_B = temp;
+  temp =  incomingPacket[4];
+  if(temp == 1 && vehicle.rotate_A == 0) vehicle.soft_stop_A = true;
+  else vehicle.soft_stop_A = false;
+  temp =  incomingPacket[5];
+  if(temp == 1 && vehicle.rotate_B == 0) vehicle.soft_stop_B = true;
+  else vehicle.soft_stop_B = false;
+  Serial.printf("Got: %d %d %d %d \n", incomingPacket[2], incomingPacket[3], incomingPacket[4], incomingPacket[5]);
+  Serial.printf("Translated: %d %d %d %d \n", vehicle.rotate_A, vehicle.rotate_B, vehicle.soft_stop_A, vehicle.soft_stop_B);
+}
 void tank_test(){
   for(int i = -1023; i < 1024; i++){
     vehicle.rotate_A = i;
@@ -107,4 +169,3 @@ void set_engines(){
   digitalWrite(D6, BIN2);
   analogWrite(D7, PWMB);
 }
-
